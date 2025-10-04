3f57bad7c86d1799d3650112bfb0a00d02851e12
vlarou7 <vpetrovic686@gmail.com>
2025-10-04 06:42:53 +0200

    Add config.py for potential use

---
+++ src/config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# --- GitHub ---
+GITHUB_TOKEN = "GITHUB_TOKEN_HERE"
+
+# --- YouTrack ---
+YOUTRACK_TOKEN = "YOUTRACK_TOKEN_HERE"
+YOUTRACK_URL = "https://theos.youtrack.cloud"
+YOUTRACK_PROJECT = "GS"
+YOUTRACK_PROJECT_ID = "0-1"
